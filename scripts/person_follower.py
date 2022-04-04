fbe1464fc810ad4300bf3491803f2680037b46d4
jason chee <jchee1@uchicago.edu>
2022-04-04 03:19:04 +0000

    initial person follow code

--- scripts/person_follower.py
+++ scripts/person_follower.py
@@ -11,28 +11,27 @@ class PersonFollower(object):
         rospy.init_node('person_follow')
         self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.scan_pub = rospy.Subscriber('/scan', LaserScan, self.process_point)
-        self.go_forward = 6
+        self.twist = Twist()
 
     def process_point(self, data):
         #go through ranges and see where value is less than 5
         #index of that value in ranges list correspond to angle we need to change 
 
         #check to see if straight ahead, if it is, then just go forward
-        if data.ranges[0] <= 5:
-            go_for = Twist()
-            go_for.linear.x = self.go_forward
-            self.vel_pub.publish(go_for)
+        if data.ranges[0] <= 5.0:      
+            self.twist.linear.x = 4
+            self.twist.angular.z = 0
+            self.vel_pub.publish(self.twist)
             return
 
         for angle, dist in enumerate(data.ranges):
-            if dist <= 5:
+            if dist <= 5.0:
                 #update movement in robot
-                update = Twist()
-                if angle < 180:
-                    update.angular.z = -1.57
-                else:
-                    update.angular.z = 1.57
-                self.vel_pub.publish(update)
+                if angle < 180: #turn left
+                    self.twist.angular.z = 1.57 #change num when test
+                else: #turn right
+                    self.twist.angular.z = -1.57
+                self.vel_pub.publish(self.twist)
                 return
                 
 
